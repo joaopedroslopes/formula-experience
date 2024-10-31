61c640f65f2e0c0d8aef4c490f0e0f58511e81b5
João Pedro de Souza Lopes <jp01.bao@gmail.com>
2024-10-30 21:34:58 -0300

    inicio do repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+		<div className="h-screen w-screen bg-dark-blue">
+			<h1 className="text-white text-3xl font-semibold">Formula experience</h1>
+			<p>teste</p>
+		</div>
+    </>
+  )
+}
+
+export default App
